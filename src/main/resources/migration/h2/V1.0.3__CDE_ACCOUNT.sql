92811b63138f2bf6fd2620aa211578b51c8d9cf3
HaifengChen <chenhaifeng@zyeeda.com>
2014-10-08 20:35:34 +0800

    support h2 database

---
+++ src/main/resources/migration/h2/V1.0.3__CDE_ACCOUNT.sql
@@ -0,0 +1,12 @@
+INSERT INTO `CDE_ACCOUNT` (`F_ID`, `F_ACCOUNT_NAME`, `F_DELETED`, `F_DISABLED`, `F_EMAIL`, `F_MOBILE`, `F_PASSWORD`, `F_REALNAME`, `F_TELEPHONE`, `F_DEPARTMENT_ID`)
+VALUES
+	('brook','brook',0,0,NULL,NULL,'brook','brook',NULL,'d-1009'),
+	('chopper','chopper',0,0,NULL,NULL,'chopper','chopper',NULL,'d-1004'),
+	('franky','franky',0,0,NULL,NULL,'franky','franky',NULL,'d-1004'),
+	('luffy','luffy',0,0,NULL,NULL,'luffy','luffy',NULL,'d-1001'),
+	('nami','nami',0,0,NULL,NULL,'nami','nami',NULL,'d-1003'),
+	('robin','robin',0,0,NULL,NULL,'robin','robin',NULL,'d-1003'),
+	('sanji','sanji',0,0,NULL,NULL,'sanji','sanji',NULL,'d-1004'),
+	('tom','tom',0,0,NULL,NULL,'tom','tom',NULL,'d-1001'),
+	('usopp','usopp',0,0,NULL,NULL,'usopp','usopp',NULL,'d-1009'),
+	('zoro','zoro',0,0,NULL,NULL,'zoro','zoro',NULL,'d-1002');
