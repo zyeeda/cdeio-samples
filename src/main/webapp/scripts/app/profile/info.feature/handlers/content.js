5c5d05260c6a2098dcf8e51c410b087ed347b55f
luqiudong <luqiudong@zyeeda.com>
2015-02-09 11:30:43 +0800

    个人信息界面样式调整

--- src/main/webapp/scripts/app/profile/info.feature/handlers/content.js
+++ src/main/webapp/scripts/app/profile/info.feature/handlers/content.js
@@ -4,73 +4,38 @@ define([
 ], function($) {
     return {
         showChangePasswordForm: function() {
-            $('button[id$="change-password-btn"]').addClass('view-hide');
-            $('button[id$="password"]').each(function(e) {
-                $(this).removeClass('view-hide');
+            $('button[id$="change-password-btn"]').hide();
+            
+            $('button[id$="quit-password"]').on('click', function() {
+                $('.info-password').slideUp('300', function() {
+                    $('.c-action-form-changePassword').remove();
+                    $('button[id$="change-password-btn"]').show();
+                });
+            });
+
+            $('button[id$="reset-password"]').on('click', function() {
+                $('input[type="password"]').attr('');
             });
             
             var app = this.feature.module.getApplication(), me = this;
             var accFeature = app.loadFeature('system/scaffold:accounts');
             accFeature.done(function(feature) {
                 var view = feature.views['form:changePassword'];
-                view.$el = $('.info-password');
+                view.$el = $('.changePassword');
                 view.render();
-
-                $('button[id$="quit-password"]').on('click', function() {
-                    $('button[id$="change-password-btn"]').removeClass('view-hide');
-                    $('button[id$="password"]').each(function(e) {
-                        $(this).addClass('view-hide');
-                    });
-                    $('.c-action-form-changePassword').remove();
-                });
-
-                $('button[id$="reset-password"]').on('click', function() {
-                    $('input[type="password"]').attr('');
-                });
-
+                $('.info-password').hide();
+                $('.info-password').slideDown('300');
+                
                 $('button[id$="submit-password"]').on('click', function() {
                     var data = view.getFormData();
                     feature.request({
-                        url: 'password', 
+                        url: 'password',
                         type: 'put',
                         data: data,
                         success: function(d) {
-                            console.log(d);
                             app.success('密码修改成功');
                     }});
                 });
-                // app.showDialog({
-                //     view: view,
-                //     title: '修改密码',
-                //     buttons: [{
-                //         label: '确定',
-                //         status: 'btn-primary',
-                //         fn: function() {
-                //             var data = view.getFormData();
-                //             // if(data.oldPassword === ''){
-                //             //     app.error('原密码不可为空');
-                //             //     return false;
-                //             // } else if(data.newPassword === ''){
-                //             //     app.error('新密码不可为空');
-                //             //     return false;
-                //             // } else if(data.newPassword.length < 6 || data.newPassword.length > 12){
-                //             //     app.error('新密码要由6到12个字符组成');
-                //             //     return false;
-                //             // } else if(data.newPassword != data.newPassword2){
-                //             //     app.error('两次密码不一致');
-                //             //     return false;
-                //             // }
-                //             feature.request({
-                //                 url: 'password', 
-                //                 type: 'put',
-                //                 data: data,
-                //                 success: function(d) {
-                //                     console.log(d);
-                //                     app.success('密码修改成功');
-                //             }});
-                //         }
-                //     }]
-                // });
             });
         },
     };
